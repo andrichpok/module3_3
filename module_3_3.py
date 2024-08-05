5484dd3ef4e3547572866e16e6c119274cdb7d13
Andrei Vanchagov <andreivanchagov@gmail.com>
2024-08-05 13:46:11 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,17 @@
+#1.Функция с параметрами по умолчанию:
+def print_params(a = 10, b = 20, c = 30):
+    print(a, b, c)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+#2.Распаковка параметров:
+
+values_list = [8, True, 'Hello']
+values_dict = {'a' : 28, 'b' : 'phone', 'c' : False}
+print_params(*values_list)
+print_params(**values_dict)
+
+#3.Распаковка + отдельные параметры:
+
+values_list_2 = [50, 'string']
+print_params(*values_list_2, 42)
